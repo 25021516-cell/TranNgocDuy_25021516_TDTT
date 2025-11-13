777e4350cc5c842aa96d8c165afef857d174e24d
25021516-cell <25021516@vnu.edu.vn>
2025-11-13 15:24:04 +0700

    f

--- HK1.py
+++ HK1.py
@@ -1,3 +1,11 @@
 #Biểu thức, Toán tử, Nhập xuất dữ lịệu
 #W2A1
 
+#W2A18
+print('W2A18 \n')
+
+print('### ##   ### ###')
+print(' #  # #   #   #')
+print(' #  #  #  #   #')
+print(' #  # #   #   #')
+print(' #  ##    #   #')
