135eab7082ba5184f5aac8022c6a1b4e829c04b6
25021516-cell <25021516@vnu.edu.vn>
2025-11-27 15:49:48 +0700

    rf

--- Bài tập cấu trúc điều khiển lựa chọn.py
+++ Bài tập cấu trúc điều khiển lựa chọn.py
@@ -193,3 +193,106 @@ def Xac_Dinh_Tam_Giac (a, b, c):
 Xac_Dinh_Tam_Giac(a_W3A11, b_W3A11, c_W3A11)
         
 print('\n')
+
+# W3A12
+print('W3A12')
+
+n_W3A12 = int(input('Nhập vào số năm: '))
+
+while (n_W3A12 < 0):
+    print('Số năm nhập vào không hợp lí lắm')
+    n_W3A12 = int(input('Nhập lại số năm: '))
+    
+if (n_W3A12 % 4 == 0 and n_W3A12 % 100 != 0):
+    print('Yes')
+else:
+    print('No')
+        
+print('\n')
+
+# W3A14
+print('W3A14')
+
+a_W3A14 = float(input('Nhập vào số thực a: '))
+b_W3A14 = float(input('Nhập vào số thực b: '))
+
+def Giai_Phuong_Trinh (a, b):
+    if a == 0:
+        if b == 0:
+            print("Phương trình vô số nghiệm")
+        else:
+            print("Phương trình vô nghiệm")
+    else:
+        x = -b / a
+        print('Phương trình có nghiệm:', x)
+    
+Giai_Phuong_Trinh(a_W3A14, b_W3A14)
+        
+print('\n')
+
+# W3A15
+print('W3A15')
+
+D_W3A15 = float(input('Nhập vào số điểm trung bình: '))
+
+while(D_W3A15 < 0 or D_W3A15 > 10):
+    print('Điểm không hoẹp lí cho lắm')
+    D_W3A15 = float(input('Nhập vào lại số điểm trung bình: '))
+    
+if (D_W3A15 >= 8):
+    print('Học sinh đạt học lực giỏi')
+elif (D_W3A15 >= 6.5):
+    print('Học sinh đạt học lực khá')
+elif (D_W3A15 >= 5):
+    print('Học sinh đạt học lực trung bình')
+else:
+    print('Học sinh đạt học lực yếu')
+    
+        
+print('\n')
+
+# W3A16
+print('W3A16')
+
+n_W3A16 = float(input('Nhập vào một số thực: '))
+
+Tron_Xuong = int(n_W3A16)
+
+if (n_W3A16 == Tron_Xuong):
+    Tron_Len = Tron_Xuong
+else:
+    Tron_Len = Tron_Xuong + 1
+
+if (n_W3A16 - Tron_Xuong < 0.5):
+    Tron_Nguyen = Tron_Xuong
+else:
+    Tron_Nguyen = Tron_Len
+
+print('Kết quả theo thứ tự làm tròn lên, tròn xuống, tròn tới số nguyên gần nhất là:', Tron_Len, Tron_Xuong, Tron_Nguyen)
+        
+print('\n')
+
+# W3A17
+print('W3A17')
+
+a_W3A17 = int(input('Nhập số a: '))
+b_W3A17 = int(input('Nhập số b: '))
+c_W3A17 = int(input('Nhập số c: '))
+d_W3A17 = int(input('Nhập số d: '))
+
+def Tim_Cap_So_Nhan (a, b, c, d):
+    if (a == 0):
+        print('Không phải cấp số nhân')
+    else:
+        if(b % a == 0):
+            q = b//a
+            if (c == b * q and d == c * q):
+                print('Đây là cấp số nhân')
+            else:
+                print('Không phải cấp số nhân')
+        else:
+            print('Không phải cấp số nhân')
+        
+Tim_Cap_So_Nhan(a_W3A17, b_W3A17, c_W3A17, d_W3A17)
+        
+print('\n')
