87435e18ef4e28885891eec3b9e3bef370a19e79
25021516-cell <25021516@vnu.edu.vn>
2026-01-07 23:31:53 +0700

    h

--- Bài tập tuần 10.py
+++ Bài tập tuần 10.py
@@ -1,3 +1,30 @@
-#9A1
-print('W9A1')
+# #9A1
+# print('W9A1')
+
+# list1_9A1 = list(map(int, input().split()) )
+# list2_9A1 = list(map(int, input().split()) )
+
+# def merge_list(list1, list2):
+#     i = j = 0
+#     merged_list = []
+
+#     while i < len(list1) and j < len(list2):
+#         if list1[i] <= list2[j]:
+#             merged_list.append(list1[i])
+#             i += 1
+#         else:
+#             merged_list.append(list2[j])
+#             j += 1
+
+#     if i < len(list1):
+#         merged_list.extend(list1[i:])
+#     if j < len(list2):
+#         merged_list.extend(list2[j:])
+
+#     return merged_list
+
+# print(merge_list(list1_9A1, list2_9A1))
+
+#9A2
+print('W9A2')
 
